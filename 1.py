58596164938ed3a6a4b8db9123107a876bdcfc1b
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2017-11-09 10:43:17 +1300

    Faster with filter

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
 
-mult35 = lambda n: sum(x for x in range(0, n) if x % 3 == 0 or x % 5 == 0);
+ismult = lambda x: x % 3 == 0 or x % 5 == 0;
+mult35 = lambda n: sum(filter(ismult, [x for x in range(n)]));
 print(mult35(1000));
